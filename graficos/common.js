77abda7290ce948d3538b75e23f2260dfe28df57
tavinho2 <162485414+tavinho2@users.noreply.github.com>
2024-10-09 14:37:51 -0300

    09/10

---
+++ graficos/common.js
@@ -0,0 +1,10 @@
+const getCSS = (VARIAVEL) => {
+    const bodyStyles = getComputedStyle(document.body)
+    return bodyStyles.getPropertyValue(VARIAVEL)
+}
+const tickConfig = {
+    family: getCSS('--font'),
+    size: 16,
+    color: getCSS('--primary-color')
+}
+export {getCSS, tickConfig}
