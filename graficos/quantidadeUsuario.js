ac6717f2d8f6b00f77f3fa790882ff9ebf490682
tavinho2 <162485414+tavinho2@users.noreply.github.com>
2024-10-09 16:39:17 -0300

    09/10 ás 16:39

--- graficos/quantidadeUsuario.js
+++ graficos/quantidadeUsuario.js
@@ -17,5 +17,43 @@ async function quantidadeUsuariosPorRede() {
             }
         }
     ]
-    
+  
+    const laytout = {
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor: getCSS('--bg-color'),
+        title: {
+            text: 'Redes sociais com mais usuarios'
+            x: 0,
+            font: {
+                color: getCSS('--primary-color'),
+                size: 30,
+                font: getCSS('--font')
+            }
+        },
+        xaxis: {
+            tickfont: tickConfig,
+            title: {
+                text:'Nome das redes sociais'
+                font: {
+
+                }
+            }
+        },
+        yaxis: {
+            tickConfig,
+            title: {
+                text: 'Bilhoes de usuarios ativos'
+                font: {
+                    color: getCSS('--secondary-color')
+                }
+            }
+        }
+    }
+
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document. getElementById('graficos-container').appendChild(grafico)
+    Ploty.newPlot(grafico, data, laytout)
 }
+
+quantidadeUsuariosPorRede()
