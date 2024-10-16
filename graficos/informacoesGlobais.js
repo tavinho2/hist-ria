12b818eb1fffb01db3045b1ef8d36371b663ff29
tavinho2 <162485414+tavinho2@users.noreply.github.com>
2024-10-16 14:09:03 -0300

    tdyt

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,20 +1,20 @@
-const url = 'https://'dev/github/tavinho2/hist-ria?vscode-lang=pt-br
+const url = 'https://'/github/tavinho2/hist-ria?vscode-
 
-async function vizualizarInformacoesGlobais(){
-    const res = await fetch(url)
-    const dados = await res.json()
-    const pessoasConectadas = (dados. total_pessoas_conectadas / 1e9)
-    const pessoasNoMundo =(dados.total_pessoas_mundo / 1e9)
-    const horas = parseInt(dados.tempo_medio)
-    const minutos = Math.round((dados.tempo_medio - horas)*100)
-    const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo)*100).toFixed
+    async () => {
+        const res = await fetch(url)
+        const dados = await res.json()
+        const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+        const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+        const horas = parseInt(dados.tempo_medio)
+        const minutos = Math.round((dados.tempo_medio - horas) * 100)
+        const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo) * 100).toFixed
 
- const paragrafo = document.createElement('p')
-paragrafo.classList.add('graficos-container_texto')
-paragrafo.innerHTML = 'Voce sabia que o mundo tem <span>${pessoasNoMundo}'
+        const paragrafo = document.createElement('p')
+        paragrafo.classList.add('graficos-container_texto')
+        paragrafo.innerHTML = 'Voce sabia que o mundo tem <span>${pessoasNoMundo}'
 
-const container = document.getElementById('graficos-container')
-container.appendChild(paragrafo)
-}
+        const container = document.getElementById('graficos-container')
+        container.appendChild(paragrafo)
+    }
 
 vizualizarInformacoesGlobais()
